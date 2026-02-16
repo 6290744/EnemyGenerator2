1a62bf9edafc13f5fcf49ffbc66f899db19cc59f
ERisunkov <6290744@gmail.com>
2026-02-16 20:36:27 +0100

    Renamed position method

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    public Vector3 GetSpawnPosition()
+    public Vector3 GetPosition()
     {
         return transform.position;
     }
