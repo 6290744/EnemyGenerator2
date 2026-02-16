77a9fc67885b2edc10dd93521aa7210b6ce5a9fb
ERisunkov <6290744@gmail.com>
2026-02-16 22:35:47 +0100

    Сreated spawner invoker

---
+++ Assets/Scripts/SpawnInvoker.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpawnInvoker : MonoBehaviour
+{
+    [SerializeField] private EnemySpawner[]  _spawners;
+    [SerializeField] private int _spawnLatency = 2;
+    
+    private bool _isSpawning;
+
+    private void Start()
+    {
+        _isSpawning = true;
+        StartCoroutine(Spawnloop());
+    }
+
+    private IEnumerator Spawnloop()
+    {
+        while (_isSpawning)
+        {
+            yield return new WaitForSeconds(_spawnLatency);
+            
+            GetRandomSpawner().SpawnEnemy();
+        }
+    }
+
+    private EnemySpawner GetRandomSpawner()
+    {
+        return _spawners[Random.Range(0, _spawners.Length)];
+    }
+}
