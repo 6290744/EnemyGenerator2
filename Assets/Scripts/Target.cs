2ebcb47562a048bcbf05500cd0e130db7968665c
ERisunkov <6290744@gmail.com>
2026-02-20 11:16:38 +0100

    Moving script added to new class

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,34 +1,6 @@
-using System.Collections;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    [SerializeField] private GameObject _centerOfRotation;
-    [SerializeField] private float _rotationSpeed = 10;
-
-    private bool _isMooving;
-
-    private void OnEnable()
-    {
-        _isMooving = true;
-
-        StartCoroutine(Moveloop());
-    }
-
-    public Vector3 GetPosition()
-    {
-        return transform.position;
-    }
-
-    private IEnumerator Moveloop()
-    {
-        WaitForEndOfFrame waitForEndOfFrame = new WaitForEndOfFrame();
-        
-        while (_isMooving)
-        {
-            transform.RotateAround(_centerOfRotation.transform.position, Vector3.up, _rotationSpeed * Time.deltaTime);
-
-            yield return waitForEndOfFrame;
-        }
-    }
+    public Vector3 Position => transform.position;
 }
